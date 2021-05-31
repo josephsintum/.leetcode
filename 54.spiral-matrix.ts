db1656a9b2022ac6b8f24fef92a2c36684ba019b
josephsintum <sjumbam15@gmail.com>
2021-05-31 15:09:09 -0700

    submission runtime 89.98% memory 82.20%

--- 54.spiral-matrix.ts
+++ 54.spiral-matrix.ts
@@ -6,7 +6,47 @@
 
 // @lc code=start
 function spiralOrder(matrix: number[][]): number[] {
-    return matrix.flat()
+
+    // if matrix is empty, or has 1 or 2 elements then return flattened matrix
+    if (matrix.length < 2) return matrix.flat();
+
+    let spiral = [];
+
+    let top = 0;
+    let bottom = matrix.length - 1;
+    let left = 0;
+    let right = matrix[0].length - 1;
+    let size = matrix.length * matrix[0].length
+
+    // check if spiral is complete
+    while (spiral.length < size) {
+
+        // top
+        for (let i = left; i <= right && spiral.length < size; i++)
+            spiral.push(matrix[top][i])
+        top++;
+
+        // right
+        for (let i = top; i <= bottom && spiral.length < size; i++)
+            spiral.push(matrix[i][right])
+        right--;
+
+        // bottom
+        for (let i = right; i >= left && spiral.length < size; i--)
+            spiral.push(matrix[bottom][i])
+        bottom--;
+
+        // left
+        for (let i = bottom; i >= top && spiral.length < size; i--)
+            spiral.push(matrix[i][left])
+        left++;
+
+    }    
+
+    return spiral
 };
 // @lc code=end
 
+let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+spiralOrder(matrix)
