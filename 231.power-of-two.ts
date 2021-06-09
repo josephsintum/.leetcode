c3a653d4370eea9931b38d3d9cca9b73973d9aca
josephsintum <sjumbam15@gmail.com>
2021-06-08 18:54:36 -0700

    log2() solution

--- 231.power-of-two.ts
+++ 231.power-of-two.ts
@@ -8,16 +8,15 @@
 function isPowerOfTwo(n: number): boolean {
 
     if (n < 1) return false // if 0 for negative numbers
-    if (n <= 2) return true 
     // if n is divisible by 2
-    else if (n % 2 == 0) return isPowerOfTwo(n / 2)
+    else if (Math.log2(n) % 1 == 0) return true
     return false;
 
 };
 // @lc code=end
 
 // Submission report
-// Your runtime beats 20 % of typescript submissions
-// Your memory usage beats 8.89 % of typescript submissions (40.9 MB)
+// Your runtime beats 66.67 % of typescript submissions
+// Your memory usage beats 66.67 % of typescript submissions (40.3 MB)
 
-console.log(isPowerOfTwo(-16))
+console.log(isPowerOfTwo(1))
