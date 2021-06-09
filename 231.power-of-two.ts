f408002d5ced2e028f55bf084727ae4d0f011c67
josephsintum <sjumbam15@gmail.com>
2021-06-08 18:47:22 -0700

    recursiuve approach

--- 231.power-of-two.ts
+++ 231.power-of-two.ts
@@ -7,10 +7,17 @@
 // @lc code=start
 function isPowerOfTwo(n: number): boolean {
 
-    
+    if (n < 1) return false // if 0 for negative numbers
+    if (n <= 2) return true 
+    // if n is divisible by 2
+    else if (n % 2 == 0) return isPowerOfTwo(n / 2)
+    return false;
 
-    return true;
 };
 // @lc code=end
 
-console.log(isPowerOfTwo(4))
+// Submission report
+// Your runtime beats 20 % of typescript submissions
+// Your memory usage beats 8.89 % of typescript submissions (40.9 MB)
+
+console.log(isPowerOfTwo(-16))
