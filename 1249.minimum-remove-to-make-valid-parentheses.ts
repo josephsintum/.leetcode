7d7a0e9c2a1c0cad52a2666c0bada96ad492654b
josephsintum <sjumbam15@gmail.com>
2021-06-08 18:20:41 -0700

    new challenge

---
+++ 1249.minimum-remove-to-make-valid-parentheses.ts
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1249 lang=typescript
+ *
+ * [1249] Minimum Remove to Make Valid Parentheses
+ */
+
+// @lc code=start
+function minRemoveToMakeValid(s: string): string {
+
+    
+
+    return s
+};
+// @lc code=end
+
---
+++ 231.power-of-two.ts
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=231 lang=typescript
+ *
+ * [231] Power of Two
+ */
+
+// @lc code=start
+function isPowerOfTwo(n: number): boolean {
+
+    
+
+    return true;
+};
+// @lc code=end
+
+console.log(isPowerOfTwo(4))
