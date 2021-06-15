c118e6fc122836f87e785021664d3d48b25ef9ff
josephsintum <sjumbam15@gmail.com>
2021-06-14 20:26:50 -0700

    submission

--- 9.palindrome-number.ts
+++ 9.palindrome-number.ts
@@ -6,7 +6,19 @@
 
 // @lc code=start
 function isPalindrome(x: number): boolean {
+    if (x < 0) return false
+    else if (x < 10) return true
 
+    let num = new String(x)
+    let j = num.length - 1
+    for (let i = 0; i < num.length / 2; i++) {
+        if (num[i] == num[j]) j--;
+        else return false
+    }
+    return true
 };
 // @lc code=end
 
+// Your runtime beats 65.53 % of typescript submissions
+// Your memory usage beats 52.33 % of typescript submissions (49.4 MB)
+console.log(isPalindrome(11)) // true
